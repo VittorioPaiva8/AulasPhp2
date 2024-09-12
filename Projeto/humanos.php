70beccde12463eab20c5c89abf5f702df4e5f5ae
VittorioPaiva <vittoriopaiva@gmail.comgit config --global user.name VittorioPaivagit config --global user.email vittoriopaiva@gmail.com>
2024-09-12 11:35:23 -0300

    atualizado

--- Projeto/humanos.php
+++ Projeto/humanos.php
@@ -6,4 +6,13 @@ class FichaTecnica
     public $idade = 18;
     public $endereco = '';
     public $contato = '';
+
+
+    function __construct($nome1, $idade = 18, $endereco, $contato)
+    {
+        $this->nome = $nome1;
+        $this->idade = $idade;
+        $this->endereco = $endereco;
+        $this->contato = $contato;
+    }
 }
